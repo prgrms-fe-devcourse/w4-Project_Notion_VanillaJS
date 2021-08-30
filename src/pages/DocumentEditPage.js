210c3969272823d660b7d5b68ca77f68e27f6a25
aB <rkdvnfma90@hanmail.net>
2021-08-30 11:33:59 +0900

    feat DocumentEditPage 페이지

---
+++ src/pages/DocumentEditPage.js
@@ -0,0 +1,42 @@
+import DocumentEditor from '../components/DocumentEditor.js';
+import ChildDocumentLink from '../components/ChildDocumentLink.js';
+import { fetchGetDocument } from '../utils/api.js';
+import { checkIsEmptyThrowError, checkUseConstructorFunction } from '../utils/validator.js';
+import { CLASS_NAME_EDIT_PAGE } from '../utils/constants.js';
+import { createElement } from '../utils/dom.js';
+
+export default function DocumentEditPage({ $target, initialState }) {
+  const validate = state => {
+    checkUseConstructorFunction(new.target, DocumentEditPage);
+    checkIsEmptyThrowError(state);
+  };
+
+  validate(initialState);
+  this.state = initialState;
+
+  const $page = createElement('div');
+  $page.className = CLASS_NAME_EDIT_PAGE;
+
+  const documentEditor = new DocumentEditor({
+    $target: $page,
+    initialState: {},
+  });
+
+  const childDocumentLink = new ChildDocumentLink({ $target: $page, initialState: [] });
+
+  this.setState = async nextState => {
+    validate(nextState);
+    const documentInfo = await fetchGetDocument(nextState);
+    documentEditor.setState(documentInfo);
+    childDocumentLink.setState(documentInfo.documents);
+    this.render();
+  };
+
+  this.clear = () => {
+    $page.remove();
+  };
+
+  this.render = () => {
+    $target.appendChild($page);
+  };
+}
