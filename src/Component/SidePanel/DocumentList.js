515b52d7dddf1f38d7d7cb290bd5211c9c7ffa65
minsgy <alstjrchl97@naver.com>
2021-08-31 00:40:11 +0900

    refactor : 문서 리스트 하위 메뉴 Toggle 이벤트 처리 및 클릭/호버 이벤트 처리 완료

--- src/Component/SidePanel/DocumentList.js
+++ src/Component/SidePanel/DocumentList.js
@@ -1,9 +1,9 @@
-import { push } from "../router.js";
 export default function DocumentList({
   $target,
   init = [],
   onSelect,
   onCreate,
+  onRemove,
 }) {
   const $documentList = document.createElement("div");
   $documentList.className = "document-list-container";
@@ -14,10 +14,19 @@ export default function DocumentList({
     return parentsDocuments
       .map(
         ({ id, title, documents }) =>
-          `<li class='document-item' data-state="folded" data-id="${id}">${title}
+          `<li class='document-item' data-id="${id}">
+          <button class="document-item__button folder">►</button>
+          ${title}
+          <button class="document-item__button add fold">+</button>
+          <button class="document-item__button remove fold">-</button>
           ${
-            documents.length > 0 ? `<ul>${renderDocuments(documents)}</ul>` : ""
+            documents.length > 0
+              ? `<ul class='sub-document fold'>${renderDocuments(
+                  documents
+                )}</ul>`
+              : `<ul class='sub-document fold'><li class="document-item sub-document__none-guide">하위 문서가 없습니다.</li></ul> `
           }
+          </li>
       `
       )
       .join("");
@@ -30,28 +39,52 @@ export default function DocumentList({
   };
   this.render = () => {
     $documentList.innerHTML = `
-        <ul>
+        <ul class="root">
         ${this.state.length > 0 && renderDocuments(this.state)}
         </ul>
         `;
   };
 
+  // mouse over 및 out 시 문서 추가버튼 구현
+  $documentList.addEventListener("mouseover", (e) => {
+    e.target.style.background = "#EFEFEF";
+    const $li = e.target.closest(".document-item");
+    const $addButton = $li.querySelector(".add");
+    const $removeButton = $li.querySelector(".remove");
+    if ($addButton || $removeButton) {
+      $addButton.classList.toggle("fold");
+      $removeButton.classList.toggle("fold");
+    }
+  });
+  $documentList.addEventListener("mouseout", (e) => {
+    e.target.style.background = "#F7F6F3";
+    const $li = e.target.closest(".document-item");
+    const $addButton = $li.querySelector(".add");
+    const $removeButton = $li.querySelector(".remove");
+    if ($addButton || $removeButton) {
+      $addButton.classList.toggle("fold");
+      $removeButton.classList.toggle("fold");
+    }
+  });
+
   $documentList.addEventListener("click", async (e) => {
     const $li = e.target.closest(".document-item");
-    if ($li) {
-      const { id } = $li.dataset;
-      // console.log(id);
-      push(`/documents/${id}`);
-      // const $childDocumentList = document.createElement("ul");
-      // const childDocument = await request(`/documents/${id}`);
-      // console.log(childDocument);
-      // $childDocumentList.innerHTML = `
-      //   ${childDocument.documents
-      //     .map(({ id, title }) => `<li data-id="${id}">${title}</li>`)
-      //     .join("")}
-      // `;
-      // $li.appendChild($childDocumentList);
-      // onSelect($li);
+    const { id } = $li.dataset;
+    const { className } = e.target;
+    if (className === "document-item") {
+      onSelect(id);
+    } else if (className === "document-item__button add") {
+      onCreate(id);
+    } else if (className === "document-item__button folder") {
+      const $ul = $li.querySelector(".sub-document");
+      if ($ul) {
+        e.target.style.transform = $ul.classList.contains("fold")
+          ? "rotate(90deg)"
+          : "";
+        $ul.classList.toggle("fold");
+      }
+    } else if (className === "document-item__button remove") {
+      onRemove(id);
     }
   });
 
