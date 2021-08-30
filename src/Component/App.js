e30834810596bdc958f258acd807bfc29bf6606b
minsgy <alstjrchl97@naver.com>
2021-08-31 00:35:00 +0900

    refactor : app.js router 메인 처리

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,23 +1,17 @@
 import { initRouter } from "./router.js";
 import MainPage from "./MainPanel/MainPage.js";
 import SidePage from "./SidePanel/SidePage.js";
-
+import GuidePage from "./MainPanel/GuidePage.js";
 export default function App({ $target }) {
   const sidePage = new SidePage({ $target });
-  const mainPage = new MainPage({
-    $target,
-    init: [],
-  });
-
+  const mainPage = new MainPage({ $target });
   this.route = () => {
-    // $target.innerHTML = ``;
     const { pathname } = location;
     if (pathname === "/") {
-      // sidePage.setState();
+      mainPage.setState({ id: null });
     } else if (pathname.indexOf("/documents/") === 0) {
       const [, , documentId] = pathname.split("/");
-      // console.log(documentId);
-      mainPage.setState({ documentId });
+      mainPage.setState({ id: documentId });
     }
   };
 
