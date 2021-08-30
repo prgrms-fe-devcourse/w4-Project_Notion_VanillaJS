22f7d34f2e3dbef03e00fdbb3140362a135827ce
minsgy <alstjrchl97@naver.com>
2021-08-31 00:39:32 +0900

    feat : Editor 자동 저장 기능 및 에디터 헤더/바디/서브 도큐먼트리스트 적용 완료

--- src/Component/MainPanel/MainPage.js
+++ src/Component/MainPanel/MainPage.js
@@ -1,10 +1,85 @@
 import { request } from "../api.js";
+import { push } from "../router.js";
+import Editor from "./Editor.js";
+import EditorTitle from "./EditorTitle.js";
+import SubDocumentList from "./SubDocumentList.js";
 
 export default function MainPage({ $target }) {
   const $mainPageContainer = document.createElement("div");
   $mainPageContainer.className = "main-page-container";
-  $mainPageContainer.textContent = "MAIN";
   $target.appendChild($mainPageContainer);
+
+  this.state = {
+    id: null,
+    title: "",
+    content: "",
+    documents: [],
+  };
+  this.setState = async (nextState) => {
+    if (nextState.id === null) {
+      // guide.render();
+    } else if (this.state.id !== nextState.id) {
+      const contentList = await request(`/documents/${nextState.id}`);
+      this.state = contentList;
+      editorTitle.setState(this.state);
+      editor.setState(this.state);
+      subDocumentList.setState(this.state);
+
+      console.log(this.state);
+      this.render();
+    }
+  };
+
+  const editorTitle = new EditorTitle({
+    $target: $mainPageContainer,
+    init: this.state,
+  });
+
+  let timer = null;
+
+  const editor = new Editor({
+    $target: $mainPageContainer,
+    init: {
+      title: "",
+      content: "",
+    },
+    onEdit: async (selectState) => {
+      if (selectState.title.length > 13) {
+        alert(
+          "제목이 13자 이상을 초과하여, 초과 내용은 자동 삭제후 저장됩니다."
+        );
+        selectState.title = selectState.title.slice(0, 14);
+      }
+      if (timer !== null) {
+        clearTimeout(timer);
+      }
+      // 자동 저장구현
+      timer = setTimeout(async () => {
+        await request(`/documents/${selectState.id}`, {
+          method: "PUT",
+          body: JSON.stringify({
+            title: selectState.title,
+            content: selectState.content,
+          }),
+        });
+        console.log(`저장완료! ${selectState.id}`);
+        push(`/documents/${selectState.id}`);
+      }, 2000);
+    },
+  });
+
+  const subDocumentList = new SubDocumentList({
+    $target: $mainPageContainer,
+    init: this.state,
+    onClick: (selectDocumentId) => {
+      push(`/documents/${selectDocumentId}`);
+    },
+  });
+
+  this.render = () => {
+    // editorTitle.setState(this.state);
+  };
+
   // const fetchDocumentAdd = async () => {
   //   const res = await request("/documents", {
   //     method: "POST",
