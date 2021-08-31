943326329e1e8af59951a7be1c2452ba463db311
minsu-zip <alstn4814@gmail.com>
2021-08-31 13:55:26 +0900

    feat : NotionPage 라우터 기능 개발

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+import NotionPage from "./NotionPage.js";
+
 export default function App({ $target }) {
-  // $target.appendChild()
+  const notionPage = new NotionPage({ $target });
+
+  this.route = () => {
+    $target.innerHTML = "";
+    const { pathname } = window.location;
+    if (pathname === "/") {
+      notionPage.setState();
+    }
+  };
+
+  this.route();
 }
