baf194b3d362af996af8e7803003c80712b0f55c
ProgWon <dhwhc0711@naver.com>
2021-08-31 15:57:53 +0900

    [feat]: add onClick function to documentList and isLoad key to documentEditor initialState
    Add onClick function to documentList to get the document mathces the id
    and Add isLoad key for no render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import PostEditor from './PostEditor.js';
-import PostList from './PostList.js';
+import DocumentEditor from './DocumentEditor.js';
+import DocumentList from './DocumentList.js';
+import { request } from './api.js';
 
 export default function App({ $target, initialState }) {
-  const postList = new PostList({
-    $target
+  const documentList = new DocumentList({
+    $target,
+    inititalState: initialState,
+    onClick: e => {
+      if (e.target.nodeName === 'SPAN') {
+        const documentId = e.target.closest('li').dataset.id;
+        documentEditor.getDocument(documentId);
+      }
+    }
   });
 
-  const postEditor = new PostEditor({
-    $target
+  const documentEditor = new DocumentEditor({
+    $target,
+    initialState: {
+      title: '',
+      content: '',
+      isLoad: true
+    }
   });
 }
