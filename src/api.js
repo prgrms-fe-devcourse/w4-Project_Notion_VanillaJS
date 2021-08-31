08420576ad955b6ccd17e1ad3a04a647cd1bcbcd
ProgWon <dhwhc0711@naver.com>
2021-08-31 15:50:56 +0900

    [refactor]: edit default value
    Edit default value of request function parameter

--- src/api.js
+++ src/api.js
@@ -1,19 +1,20 @@
 const API_END_POINT = 'https://kdt.roto.codes/documents';
 
-export const request = async (url, options = {}) => {
+export const request = async (url = '', options = {}) => {
   try {
-    const res = fetch(`${API_END_POINT}${url}`, {
+    const res = await fetch(`${API_END_POINT}${url}`, {
       ...options,
-      'Content-Type': 'application/json',
-      'x-username': 'progwon'
+      headers: {
+        'Content-Type': 'application/json',
+        'x-username': 'progwon'
+      }
     });
 
     if (res.ok) {
-      const json = await res.json();
-      return json;
+      return res.json();
     }
 
-    throw new Error('API 호출 오류');
+    throw new Error();
   } catch (e) {
     alert(e.message);
   }
