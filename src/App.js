93275846370f05da4e424371e23720491bcab14f
minsu-zip <alstn4814@gmail.com>
2021-09-01 16:10:39 +0900

    refactor : NotionPage, NotionEditPage 분리

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import NotionPage from "./NotionPage.js";
 import NotionEditPage from "./NotionEditPage.js";
-import { initRouter } from "./router.js";
-import { request } from "./Api.js";
-
+import NotionPage from "./NotionPage.js";
 export default function App({ $target }) {
   const notionPage = new NotionPage({
     $target,
-    newDoument: (documentId) => {
-      // const createPost = request("/documents", {
-      //   method: "POST",
-      //   body: JSON.stringify(documentPage),
-      // });
-      console.log(documentId);
-      notionEditPage.setState({ documentId });
+    editDocument: (id) => {
+      notionEditPage.setState(id);
     },
   });
-  const notionEditPage = new NotionEditPage({
-    $target,
-    initialState: {
-      documentId: null,
-    },
-  });
-  this.route = () => {
-    $target.innerHTML = "";
-    const { pathname } = window.location;
-    notionPage.setState();
-    notionEditPage.setState();
-    // if (pathname === "/") {
-    //   notionPage.setState();
-    // } else if (pathname.indexOf("/document/") === 0) {
+  const notionEditPage = new NotionEditPage({ $target, initialState: null });
 
-    //   const [, , documentId] = pathname.split("/");
-    //   notionEditPage.setState({ documentId });
-    // }
-  };
-
-  this.route();
-  //initRouter(() => this.route());
+  notionPage.render();
+  notionEditPage.render();
 }
