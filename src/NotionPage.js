1e1ac345b09b3a598079418121ee3d66388c1066
minsu-zip <alstn4814@gmail.com>
2021-09-02 01:33:00 +0900

    feat : 생성된 document id 찾기
    body : 신규로 생성된 id를 찾기 위해 부모id 기준으로 서버에서 요청 후 신규 id 찾아서 edit작업 처리

--- src/NotionPage.js
+++ src/NotionPage.js
@@ -8,20 +8,32 @@ export default function NotionPage({ $target, editDocument }) {
   const notionList = new NotionList({
     $target: $page,
     initialState: [],
-    newDocument: (id, tagName) => {
-      console.log(id, tagName);
+
+    //새로운 documemt 생성 후 생성된 id값을 찾아서 리턴
+    newDocument: async (id, className) => {
       //Button 클릭시 document생성
-      if (tagName === "BUTTON") {
+      if (className === "plus") {
         const post = {
           title: "기본값",
           parent: id,
         };
-        createDocument(post);
+        await createDocument(post);
+
+        const selectedDocumentId = await selectedDocument(id);
+        editDocument(selectedDocumentId);
+        await fetchDocuments();
       }
       //Li 클릭시 document 수정
       else {
         editDocument(id);
       }
+    },
+
+    onRemove: async (id) => {
+      await request(`/documents/${id}`, {
+        method: "DELETE",
+      });
+      await fetchDocuments();
     },
   });
 
@@ -37,6 +49,13 @@ export default function NotionPage({ $target, editDocument }) {
     });
 
     await fetchDocuments();
+  };
+
+  //새로운 document id값 찾기
+  const selectedDocument = async (id) => {
+    const post = await request(`/documents/${id}`);
+    const newDocumentId = post.documents.length;
+    return parseInt(post?.documents[newDocumentId - 1].id);
   };
 
   this.render = async () => {
