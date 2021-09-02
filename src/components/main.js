f4579a3bd5270544ede977e62e47be5c03f4a7c1
Shinhyojeong <hyojeonge0@naver.com>
2021-09-02 14:11:53 +0900

    feat : 기본 구조 생성

---
+++ src/components/main.js
@@ -0,0 +1,27 @@
+const DUMMY_DATA = [
+    {
+      "id": 1, // Document id
+      "title": "노션을 만들자", // Document title
+      "documents": [
+        {
+          "id": 2,
+          "title": "블라블라",
+          "documents": [
+            {
+              "id": 3,
+              "title": "함냐함냐",
+              "documents": []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 4,
+      "title": "hello!",
+      "documents": []
+    }
+  ]
+
+const $target = document.querySelector('#app')
+
