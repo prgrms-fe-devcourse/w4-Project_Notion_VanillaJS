b0c715851a17b1db9451ddb9d8a379b2694ffdac
minsgy <alstjrchl97@naver.com>
2021-09-03 01:02:09 +0900

    refactor : router data값 추가해서 리팩토링 진행중

--- src/Component/router.js
+++ src/Component/router.js
@@ -2,20 +2,24 @@ const ROUTE_CHANGE_EVENT_NAME = "route-change";
 
 export const initRouter = (onRoute) => {
   window.addEventListener(ROUTE_CHANGE_EVENT_NAME, (e) => {
-    const { nextURL } = e.detail;
+    const { nextURL, type } = e.detail;
 
     if (nextURL) {
       history.pushState(null, null, nextURL);
+      onRoute();
+    } else if (nextURL && type) {
+      history.pushState(type, null, nextURL);
       onRoute();
     }
   });
 };
 
-export const push = (nextURL) => {
+export const push = (nextURL, type = "") => {
   window.dispatchEvent(
     new CustomEvent(ROUTE_CHANGE_EVENT_NAME, {
       detail: {
         nextURL,
+        type,
       },
     })
   );
