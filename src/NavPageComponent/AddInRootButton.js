4fd8adad397c76663991177d41870e62c5d351f0
alajillo <alajillo2021@gmail.com>
2021-09-03 16:03:27 +0900

    페이지 추가 텍스트 변경

--- src/NavPageComponent/AddInRootButton.js
+++ src/NavPageComponent/AddInRootButton.js
@@ -1,7 +1,7 @@
 export default function AddInRootButton({ targetElement, onCreate }) {
   const addButton = document.createElement("button");
   targetElement.appendChild(addButton);
-  addButton.textContent = "Add a Page";
+  addButton.textContent = "+ Add a Page";
   addButton.addEventListener("click", () => {
     event.stopPropagation(addButton);
     onCreate();
