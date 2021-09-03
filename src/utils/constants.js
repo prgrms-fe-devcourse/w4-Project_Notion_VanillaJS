1d70676d7e146e3f64c7174e48d7447d590bb9c4
aB <rkdvnfma90@hanmail.net>
2021-09-03 09:07:25 +0900

    refactor 상수를 객체형태로 export 하도록 변경

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,80 @@
+export const API = {
+  API_END_POINT: 'https://kdt.roto.codes',
+  API_X_USERNAME: 'rkdvnfma90',
+};
+
+export const MESSAGE = {
+  DOCUMENTS_DOES_NOT_EXIST: '문서가 아직 하나도 없네요 😥',
+  INPUT_DOCUMENT_TITLE: '문서 제목을 입력해 주세요 ✒',
+  CONFIRM_DELETE: '⚠ 정말 문서를 삭제하실건가요!? ⚠',
+  ADD_DOCUMENT: '새 문서',
+  PLACEHOLDER_EDITOR: '내용을 여기에 작성해주세요',
+  PLACEHOLDER_TITLE: '제목을 여기에 작성해주세요',
+  PLACEHOLDER_MODAL_TITLE: '문서 제목을 입력하고  👉Enter👈',
+  EMPTY_EDIT_PAGE: '문서를 작성하거나 선택해 주세요 🙏🏻',
+};
+
+export const ERROR_MESSAGE = {
+  IS_CONSTRUCTOR_FUNCTION: 'new 키워드를 사용해 주세요',
+  INVALID_TYPE_ARRAY: '배열이 아닙니다. 현재 타입 : ',
+  INVALID_TYPE: '타입이 올바르지 않습니다. 현재 타입 : ',
+  EXPECT_TYPE: '기대 타입 : ',
+  EMPTY_ARRAY: '빈 배열입니다.',
+  EMPTY_VALUE: 'state가 빈 값입니다.',
+  ERROR_OCCURRENCE: '에러가 발생하였습니다. 새로고침 후 다시 이용해 주세요 😥',
+  EMPTY_INPUT_VALUE: '빈값은 입력할 수 없어요!',
+  API_ERROR: 'API 처리중 에러가 발생했습니다. 새로고침 후 다시 시도해주세요.',
+};
+
+export const CLASS_NAME = {
+  DOCUMENT_ITEM: 'document-item',
+  DOCUMENT_ADD_BUTTON: 'document-add-button',
+  DOCUMENT_DELETE_BUTTON: 'document-delete-button',
+  DOCUMENT_TITLE: 'document-title',
+  DOCUMENT_EDITOR: 'document-editor',
+  DOCUMENT_TREE: 'document-tree',
+  DOCUMENT_LIST: 'document-list',
+  ROOT_DOCUMENT_ADD_BUTTON: 'root-document-add-button',
+  DOCUMENT_PAGE: 'documents-page',
+  EDIT_PAGE: 'edit-page',
+  CHILD_DOCUMENT_LINK: 'child-document-link',
+  CHILD_DOCUMENT_CONTAINER: 'child-document-container',
+  LAST_NODE: 'last-node',
+  NODE_LABEL: 'node-label',
+  BUTTON_GROUP: 'button-group',
+  LOGO_CONTAINER: 'logo-container',
+  LOGO_IMAGE: 'logo-image',
+  MESSAGE_CONTAINER: 'message-container',
+  DIMMED: 'dimmed',
+  MODAL: 'modal',
+  DISPLAY_FLEX: 'display-flex',
+  FA_BOOK_OPEN: 'fas',
+  SIDE_BUTTON: 'side-button',
+  FADE_IN: 'fade-in',
+  FADE_OUT: 'fade-out',
+  FAS: 'fas',
+  FAR: 'far',
+  MOON_ICON: 'fa-moon',
+  BULB_ICON: 'fa-lightbulb',
+  FA_ANGLE_DOUBLE_LEFT: 'fa-angle-double-left',
+  FA_ANGLE_DOUBLE_RIGHT: 'fa-angle-double-right',
+  DISPLAY_NONE: 'display-none',
+  EMPTY_EDIT_PAGE_MESSAGE: 'edit-page-empty-message',
+  EDITOR_CONTAINER: 'editor-container',
+  DARK_MODE_TOGGLE_BUTTON: 'dark-mode-toggle-button',
+};
+
+export const GENERAL = {
+  ROUTE_CHANGE_EVENT_NAME: 'route-change',
+  EDITOR_DATA_CHANGED: 'editor-data-changed',
+  DEBOUNCE_DELAY: 300,
+  KEY_ENTER: 'Enter',
+  KEY_ESCAPE: 'Escape',
+  STORAGE_KEY_NOTION_THEME: 'notion-theme',
+  DARK: 'dark',
+  LIGHT: 'light',
+};
+
 // API
 export const API_END_POINT = 'https://kdt.roto.codes';
 export const API_X_USERNAME = 'rkdvnfma90';
@@ -9,7 +86,7 @@ export const MSG_CONFIRM_DELETE = '⚠ 정말 문서를 삭제하실건가요!? 
 export const MSG_ADD_DOCUMENT = '새 문서';
 export const MSG_PLACEHOLDER_EDITOR = '내용을 여기에 작성해주세요';
 export const MSG_PLACEHOLDER_TITLE = '제목을 여기에 작성해주세요';
-export const MSG_PLACEHOLDER_MODAL_TITLE = '문서 제목을 입력하고  👉Enter👈';
+export const MSG_PLACEHOLDER_MODAL_TITLE = '문서 제목을 입력하고  👉 Enter! 👈';
 export const MSG_EMPTY_EDIT_PAGE = '문서를 작성하거나 선택해 주세요 🙏🏻';
 
 // 일반 상수
