625ad66eb61de193b3a3a7c9a065bf51a216e363
Jeong Ho <gkood2@gamil.com>
2021-09-03 20:45:00 +0900

    documentPage 추가

---
+++ src/js/DocumentContentsPage.js
@@ -0,0 +1,46 @@
+import Editor from "./Editor.js"
+import SubDocList from "./SubDocList.js"
+import { makeSubDocList } from "./listHandler.js"
+export default function DocumentContentsPage({
+  $target,
+  initialState,
+  onEditing,
+  onRemove,
+}) {
+  const $page = document.createElement("article")
+  $page.className = "docPage"
+  const $removeButton = document.createElement("button")
+  $removeButton.textContent = "문서 삭제"
+  $removeButton.className = "removeBtn"
+  this.state = initialState
+  //page setState
+  this.setState = (nextState) => {
+    if (nextState.id === "remove" && $target.childNodes[1]) {
+      $target.removeChild($page)
+      return
+    }
+    this.state = nextState
+    editor.setState(this.state)
+    // subDocList.setState(makeSubDocList([this.state], this.state.id, []))
+    $page.appendChild($removeButton)
+    this.render()
+  }
+  //editor
+  const editor = new Editor({
+    $target: $page,
+    initialState,
+    onEditing,
+  })
+  // 미완성
+  // const subDocList = new SubDocList({
+  //   $target: $page,
+  //   initialState,
+  // })
+  this.render = () => {
+    if (this.state.id && this.state.id !== "remove") $target.appendChild($page)
+  }
+  $removeButton.addEventListener("click", () => {
+    onRemove(this.state.id)
+  })
+  this.render()
+}
