d92ff9f84cd1562329ded0488439b7f5382360be
JunHyeongB <aowlr0513@gmail.com>
2021-09-03 14:58:06 +0900

    update: 사용자 표시 Header

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+export default function Header({ $target, initialState }) {
+  const $header = document.createElement("div");
+  $header.className = "header";
+  $target.appendChild($header);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    $header.innerHTML = `<span class="username">${this.state.name}</span>${this.state.name}의 <span class="notion">Notion</span>`
+  }
+  
+  this.render();
+}
