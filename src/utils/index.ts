2406336ed6bc798c2e26671a6b8079eb44d208af
Baek2back <dumbuk12@naver.com>
2021-09-03 19:06:00 +0900

    keyUp 이벤트 시 디바운싱을 위한 함수 정의

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,16 @@
 export function identity<T>(arg: T): T {
   return arg;
 }
+
+export function debounce<Params extends any[]>(
+  func: (...args: Params) => void,
+  timeout: number
+): (...args: Params) => void {
+  let timer: NodeJS.Timeout;
+  return (...args: Params) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, timeout);
+  };
+}
