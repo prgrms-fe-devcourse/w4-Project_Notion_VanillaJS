cda9dcc2405c3433f643268efd7f4c75fa873766
Shinhyojeong <hyojeonge0@naver.com>
2021-09-03 15:58:20 +0900

    feat : Editor.js 추가로 문서 편집 기능 추가

---
+++ src/components/Editor.js
@@ -0,0 +1,47 @@
+export default function Editor({
+    $target,
+    initialState = {
+        titile : '',
+        content : ''
+    },
+    onEditing
+}) {
+    const $editor = document.createElement('div')
+
+    $target.appendChild($editor)
+    let isinitialize = false
+
+    this.state = initialState
+
+    this.setState = (nextState) => {
+        this.state = nextState
+        this.render()
+    }
+
+    this.render = () => {
+        if(!isinitialize){
+            $editor.innerHTML = `
+            <input type ="text" name = "title" class="editor-title" value = "${this.state.title}"/>
+            <textarea name ="content" class="editor-content">${this.state.content}</textarea>
+        `
+        }
+        isinitialize = true
+    }
+
+    this.render()
+
+    $editor.addEventListener('keyup', e=>{
+        const {target} = e
+        const name = target.getAttribute('name')
+        if(this.state[name]){
+            const nextState = {
+                ...this.state,
+                [name] : target.value
+            }
+
+            this.setState(nextState)
+            onEditing(this.state)
+        }
+    })
+
+}
--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,20 @@
 import App from "./App.js"
+import Editor from "./Editor.js"
 
 const $target = document.querySelector('#app')
 
-new App({
-  $target
+// new App({
+//   $target
+// })
+
+new Editor({
+  $target,
+  initialState:{
+    title: 'hi',
+    content : 'hello'
+  },
+  onEditing : (post) => {
+    console.log(post)
+  }
 })
-
-
 
