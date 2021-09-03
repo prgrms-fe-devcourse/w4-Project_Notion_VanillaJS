dc66eea07a22a3a6ebe99e4026f6448b496159ae
JunHyeongB <aowlr0513@gmail.com>
2021-09-03 18:52:09 +0900

    feat: PostsPage 삭제, 하위 Documents id 찾기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,35 +1,69 @@
 import Header from "./Header.js";
-import PostsPage from "./PostsPage.js";
+import PostList from "./PostList.js";
 import PostEditPage from "./PostEditPage.js";
 import { initRouter } from "./router.js";
+import { request } from "./api.js";
 
 export default function App({ $target }) {
   const $postListContainer = document.createElement("div");
   const $editorContainer = document.createElement("div");
   $postListContainer.className = "postList";
   $editorContainer.className = "editor";
-
   $target.appendChild($postListContainer);
   $target.appendChild($editorContainer);
+
+  let subDocuments = [];
 
   new Header({
     $target: $postListContainer,
     initialState: { name: "준형" },
   });
 
-  const postsPage = new PostsPage({
+  const postList = new PostList({
     $target: $postListContainer,
+    initialState: [],
+    onClickPost: (currentPosts, currentId) => {
+      subDocuments = [];
+      findSubDocuments(currentPosts, currentId);
+      postEditPage.subDocumentRender(subDocuments.reverse().slice(1));
+    }
   });
 
   const postEditPage = new PostEditPage({
     $target: $editorContainer,
     onTitleChange: () => {
-      postsPage.setState();
+      fetchPostList();
     },
   });
 
+  const findSubDocuments = (currentPosts, findId) => {
+    for (const document of currentPosts) {
+      if (document.id === parseInt(findId)) {
+        foundSubDocuments(document);
+      }
+
+      if (document.documents.length) {
+        findSubDocuments(document.documents, findId);
+      }
+    }
+  };
+
+  const foundSubDocuments = (foundDocument) => {
+    if (foundDocument.documents.length) {
+      for (const document of foundDocument.documents) {
+        foundSubDocuments(document);
+      }
+    }
+    subDocuments.push(foundDocument.id);
+  };
+
+  const fetchPostList = async () => {
+    const posts = await request("/documents");
+    postList.setState(posts);
+  };
+
   this.route = () => {
-    postsPage.setState();
+    fetchPostList();
     const { pathname } = window.location;
 
     if (pathname.indexOf("/documents/") === 0) {
