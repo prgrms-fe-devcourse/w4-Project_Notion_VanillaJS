23eb950bc427096c9b23326b66be4eea22e6fb37
grighth12 <grighth12@gmail.com>
2021-09-09 06:04:03 +0900

    refactor : TreeMenu 이벤트 위임 상위로 이동

--- src/components/Sidebar/DocumentTree.js
+++ src/components/Sidebar/DocumentTree.js
@@ -1,13 +1,7 @@
 import { renderDocumentTree } from '../../utils/templates.js'
 import { listItemClasses } from '../../constants.js'
 
-export default function DocumentTree({
-  $target,
-  initialState,
-  onDocumentClick,
-  onDocumentAdd,
-  onDocumentDelete,
-}) {
+export default function DocumentTree({ $target, initialState }) {
   const $documentTree = document.createElement('div')
 
   $target.appendChild($documentTree)
@@ -22,41 +16,6 @@ export default function DocumentTree({
   this.render = () => {
     $documentTree.innerHTML = renderDocumentTree(this.state)
   }
-
-  $documentTree.addEventListener('click', (e) => {
-    const { DOCUMENT, DELETE_BUTTON, ADD_BUTTON, COLLAPSE_BUTTON } =
-      listItemClasses
-
-    const { className } = e.target
-
-    switch (className) {
-      case DOCUMENT: {
-        const $li = e.target.closest('li')
-        const { id } = $li.dataset
-        onDocumentClick(Number(id))
-
-        break
-      }
-      case DELETE_BUTTON: {
-        const $li = e.target.closest('li')
-        const { id } = $li.dataset
-        onDocumentDelete(Number(id))
-        break
-      }
-      case ADD_BUTTON: {
-        const $li = e.target.closest('li')
-
-        if ($li) {
-          const { id } = $li.dataset
-
-          onDocumentAdd(Number(id))
-        } else {
-          onDocumentAdd(null)
-        }
-        break
-      }
-    }
-  })
 
   this.render()
 }
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,3 +1,4 @@
+import { listItemClasses } from '../../constants.js'
 import DocumentTree from './DocumentTree.js'
 import Header from './Header.js'
 /*
@@ -27,13 +28,45 @@ export default function Sidebar({
   const documentTree = new DocumentTree({
     $target: $sidebar,
     initialState: this.state.documents,
-    onDocumentClick,
-    onDocumentDelete,
-    onDocumentAdd,
   })
 
   this.setState = (nextState) => {
     this.state = nextState
     documentTree.setState(this.state.documents)
   }
+
+  $sidebar.addEventListener('click', (e) => {
+    const { DOCUMENT, DELETE_BUTTON, ADD_BUTTON, COLLAPSE_BUTTON } =
+      listItemClasses
+
+    const { className } = e.target
+
+    switch (className) {
+      case DOCUMENT: {
+        const $li = e.target.closest('li')
+        const { id } = $li.dataset
+        onDocumentClick(Number(id))
+
+        break
+      }
+      case DELETE_BUTTON: {
+        const $li = e.target.closest('li')
+        const { id } = $li.dataset
+        onDocumentDelete(Number(id))
+        break
+      }
+      case ADD_BUTTON: {
+        const $li = e.target.closest('li')
+
+        if ($li) {
+          const { id } = $li.dataset
+
+          onDocumentAdd(Number(id))
+        } else {
+          onDocumentAdd(null)
+        }
+        break
+      }
+    }
+  })
 }
