0b0702f88e2783188d0eef6162f73faaaf59eb32
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-09-09 12:54:43 +0900

    Update NotionPage.js

--- js/NotionPage.js
+++ js/NotionPage.js
@@ -1,6 +1,8 @@
 import { request } from "./api.js";
+import NotionEditPage from "./NotionEditPage.js";
 import NotionList from "./NotionList.js";
 import { push } from "./router.js";
+
 
 export default function NotionPage({ $target }) {
     const $page = document.createElement("div");
@@ -11,7 +13,7 @@ export default function NotionPage({ $target }) {
         onAdd: async (parentId) => {
             const doc = {
                 title: "새 항목",
-                parent: parentId,
+                parent: parentId
             };
             const createdDoc = await fetchDoc(doc);
             push(`/documents/${createdDoc.id}`);
@@ -23,14 +25,36 @@ export default function NotionPage({ $target }) {
             push('/');
         },
         onDocClick: (id) => {
-            push(`/documents/${id}`);
-        },
-        documentData: {}
+            const currentUrl = document.location.href;
+            const currentId = currentUrl.substring(currentUrl.lastIndexOf("/") + 1);
+            const nextId = id;
+            if (currentId !== nextId) {
+                push(`/documents/${id}`);
+            } else {
+                push(`/documents/`);
+            }
+        }
     });
 
+    const notionEditPage = new NotionEditPage({
+        $target, initialState: {
+            documentId: "new",
+            parentId: null,
+            doc: {
+                title: "",
+                content: ""
+            }
+        }
+    })
+
     this.setState = async () => {
+        const { pathname } = window.location;
+        const [, , documentId] = pathname.split("/");
         const documents = await request("/documents");
         notionList.setState(documents);
+        if (pathname.indexOf("/documents/") === 0) {
+            notionEditPage.setState({ documentId });
+        }
         this.render();
     }
 
