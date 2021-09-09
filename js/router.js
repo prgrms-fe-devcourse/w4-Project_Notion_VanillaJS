4342d7c6971d927c0c6b4e18fe62e487eae46483
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-09-09 13:14:27 +0900

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,13 +1,19 @@
 const ROUTE_CHANGE_EVENT_NAME = "route-change";
 
 export const initRouter = (onRoute) => {
+    window.onpopstate = (e) => {
+        const currentUrl = document.location.href;
+        const prevUrl = window.history.state.prevUrl;
+        history.pushState({ prevUrl: currentUrl }, null, prevUrl);
+        onRoute();
+    }
+
     window.addEventListener(ROUTE_CHANGE_EVENT_NAME, (e) => {
         const { nextUrl } = e.detail;
         if (nextUrl) {
-            history.pushState(null, null, nextUrl);
+            history.pushState({ prevUrl: nextUrl }, null, nextUrl);
             onRoute();
         }
-
     })
 }
 
